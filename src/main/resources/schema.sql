1a39036c480da054e18cff1ce0b6ac1938b7c76e
Vasily Markov <vasily@markov.biz>
2019-04-09 00:54:51 +0300

    Добавлены join-таблицы в schema.sql для поддержки более 1 телефонного номера на организацию/офис. В целях упрощения схемы БД составные первичные ключи заменены на одинарные.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,103 +1,101 @@
 /* Организация */
 CREATE TABLE IF NOT EXISTS Organization (
-    org_id           INTEGER     NOT NULL AUTO_INCREMENT COMMENT 'Уникальный идентификатор организации',
+    id               INTEGER     NOT NULL COMMENT 'Уникальный идентификатор организации' PRIMARY KEY AUTO_INCREMENT,
     version          INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
     name             VARCHAR(50) NOT NULL COMMENT 'Наименование организации',
     full_name        VARCHAR(50) NOT NULL COMMENT 'Полное наименование организации',
     inn              VARCHAR(10) NOT NULL COMMENT 'ИНН, маска [0-9]',
     kpp              VARCHAR(9)  NOT NULL COMMENT 'КПП, маска [0-9]',
     is_active        BOOLEAN     NOT NULL,
-    PRIMARY KEY (org_id),
 );
 COMMENT ON TABLE Organization IS 'Организация';
-CREATE INDEX IX_Organization_Org_id ON Organization(org_id);
+CREATE INDEX IX_Organization_Id ON Organization(id);
 
 /* Офис */
 CREATE TABLE IF NOT EXISTS Office (
-    org_id           INTEGER     NOT NULL COMMENT 'Уникальный идентификатор организации',
-    off_id           INTEGER     NOT NULL AUTO_INCREMENT COMMENT 'Уникальный идентификатор офиса (1 - головной офис)',
+    id               INTEGER     NOT NULL COMMENT 'Уник. идент. офиса (1 - головной офис)' PRIMARY KEY AUTO_INCREMENT,
     version          INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
+    org_id           INTEGER     NOT NULL COMMENT 'Уникальный идентификатор организации',
     name             VARCHAR(50) NOT NULL COMMENT 'Наименование офиса',
-    phone_id         INTEGER              COMMENT 'Уникальный идентификатор телефонного номера',
+    address          VARCHAR(50) NOT NULL COMMENT 'Адрес',
     is_active        BOOLEAN     NOT NULL,
-    PRIMARY KEY (org_id, off_id),
 );
 COMMENT ON TABLE Office IS 'Офис';
-CREATE INDEX IX_Office_Org_id_Off_id ON Office (org_id, off_id);
+CREATE INDEX IX_Office_Id ON Office (id);
 
-ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organization(org_id);
-
-/* Адрес */
-CREATE TABLE IF NOT EXISTS Address (
-    org_id           INTEGER     NOT NULL COMMENT 'Уникальный идентификатор организации',
-    off_id           INTEGER     NOT NULL COMMENT 'Уникальный идентификатор офиса',
-    version          INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
-    address          VARCHAR(50) NOT NULL COMMENT 'Адрес организации (при off_id = 1) / офиса',
-    is_active        BOOLEAN     NOT NULL,
-    PRIMARY KEY (org_id, off_id),
-);
-COMMENT ON TABLE Address IS 'Адрес';
-CREATE INDEX IX_Address_Org_id_Off_id ON Office (org_id, off_id);
-
-ALTER TABLE Address ADD FOREIGN KEY (org_id, off_id) REFERENCES Office(org_id, off_id);
+ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organization(id);
 
 /* Пользователь */
 CREATE TABLE IF NOT EXISTS User (
-    org_id           INTEGER     NOT NULL COMMENT 'Уникальный идентификатор организации',
-    off_id           INTEGER     NOT NULL COMMENT 'Уникальный идентификатор офиса',
-    usr_id           INTEGER     NOT NULL AUTO_INCREMENT COMMENT 'Уникальный идентификатор пользователя',
+    id               INTEGER     NOT NULL COMMENT 'Уникальный идентификатор пользователя' PRIMARY KEY AUTO_INCREMENT,
     version          INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
+    office_id        INTEGER     NOT NULL COMMENT 'Уникальный идентификатор офиса',
     first_name       VARCHAR(50) NOT NULL COMMENT 'Имя',
-    second_name      VARCHAR(50) NOT NULL COMMENT 'Фамилия',
-    middle_name      VARCHAR(50) NOT NULL COMMENT 'Отчество',
+    second_name      VARCHAR(50)          COMMENT 'Фамилия',
+    middle_name      VARCHAR(50)          COMMENT 'Отчество',
     position         VARCHAR(50) NOT NULL COMMENT 'Должность',
-    phone_id         INTEGER              COMMENT 'Уникальный идентификатор телефонного номера',
-    doc_code         VARCHAR(2)  NOT NULL COMMENT 'Код вида документа, маска [0-9]',
-    doc_number       VARCHAR(50) NOT NULL COMMENT 'Номер документа',
-    doc_date         DATE        NOT NULL COMMENT 'Дата документа',
+    doc_code         VARCHAR(2)           COMMENT 'Код вида документа, маска [0-9]',
+    doc_number       VARCHAR(50)          COMMENT 'Номер документа',
+    doc_date         DATE                 COMMENT 'Дата документа',
     citizenship_code VARCHAR(3)  NOT NULL COMMENT 'Код гражданства, маска [0-9]',
     is_identified    BOOLEAN     NOT NULL,
-    PRIMARY KEY (org_id, off_id, usr_id),
 );
 COMMENT ON TABLE User IS 'Пользователь';
-CREATE INDEX IX_User_Org_id_Off_id_Usr_id ON User (org_id, off_id, usr_id);
+CREATE INDEX IX_User_Id ON User (id);
 
-ALTER TABLE User ADD FOREIGN KEY (org_id, off_id) REFERENCES Office(org_id, off_id);
+ALTER TABLE User ADD FOREIGN KEY (office_id) REFERENCES Office(id);
 
 /* Номер телефона */
 CREATE TABLE IF NOT EXISTS Phone (
-    org_id           INTEGER     NOT NULL COMMENT 'Уникальный идентификатор организации',
-    off_id           INTEGER     NOT NULL COMMENT 'Уникальный идентификатор офиса',
-    phone_id         INTEGER     NOT NULL AUTO_INCREMENT COMMENT 'Уникальный идентификатор телефонного номера',
+    id               INTEGER     NOT NULL COMMENT 'Уникальный идентиф. телефонного номера' PRIMARY KEY AUTO_INCREMENT,
     version          INTEGER     NOT NULL COMMENT 'Служебное поле hibernate',
     phone            VARCHAR(15) NOT NULL COMMENT 'Телефонный номер в международном формате, маска [0-9]',
-    is_active        BOOLEAN     NOT NULL,
-    PRIMARY KEY (org_id, off_id, phone_id),
 );
 COMMENT ON TABLE Phone IS 'Телефонный номер';
-CREATE INDEX IX_Phone_Org_id_Off_id_Phone_id ON Phone (org_id, off_id, phone_id);
+CREATE INDEX IX_Phone_Id ON Phone (id);
 
-ALTER TABLE Phone ADD FOREIGN KEY (org_id, off_id) REFERENCES Office(org_id, off_id);
-ALTER TABLE Office ADD FOREIGN KEY (org_id, off_id, phone_id) REFERENCES Phone(org_id, off_id, phone_id);
-ALTER TABLE User ADD FOREIGN KEY (org_id, off_id, phone_id) REFERENCES Phone(org_id, off_id, phone_id);
+/* Join-таблица для связи Office и Phone */
+CREATE TABLE IF NOT EXISTS Office_Phone (
+    office_id        INTEGER     NOT NULL COMMENT 'Уникальный идентификатор офиса',
+    phone_id         INTEGER     NOT NULL COMMENT 'Уникальный идентификатор телефонного номера',
+    PRIMARY KEY (office_id, phone_id),
+);
+COMMENT ON TABLE Office_Phone IS 'Join-таблица для связи Office и Phone';
+CREATE INDEX IX_Phone_Office_Id ON Office_Phone (office_id);
+
+ALTER TABLE Office_Phone ADD FOREIGN KEY (phone_id) REFERENCES Phone(id);
+ALTER TABLE Office_Phone ADD FOREIGN KEY (office_id) REFERENCES Office(id);
+
+/* Join-таблица для связи User и Phone */
+CREATE TABLE IF NOT EXISTS User_Phone (
+    user_id          INTEGER     NOT NULL COMMENT 'Уникальный идентификатор пользователя',
+    phone_id         INTEGER     NOT NULL COMMENT 'Уникальный идентификатор телефонного номера',
+    PRIMARY KEY (user_id, phone_id),
+);
+COMMENT ON TABLE User_Phone IS 'Join-таблица для связи User и Phone';
+CREATE INDEX IX_Phone_User_Id ON User_Phone (user_id);
+
+ALTER TABLE User_Phone ADD FOREIGN KEY (phone_id) REFERENCES Phone(id);
+ALTER TABLE User_Phone ADD FOREIGN KEY (user_id) REFERENCES User(id);
 
 /* Классификатор документов */
-CREATE TABLE IF NOT EXISTS Doc (
+CREATE TABLE IF NOT EXISTS Document (
     code             VARCHAR(2)  NOT NULL COMMENT 'Код вида документа',
     name             VARCHAR(50) NOT NULL COMMENT 'Вид документа',
     PRIMARY KEY (code),
 );
-COMMENT ON TABLE Doc IS 'Документ';
-CREATE INDEX IX_Doc_Code ON Doc(code);
+COMMENT ON TABLE Document IS 'Документ';
+CREATE INDEX IX_Doc_Code ON Document(code);
+
+ALTER TABLE User ADD FOREIGN KEY (doc_code) REFERENCES Document(code);
 
 /* Классификатор стран мира */
 CREATE TABLE IF NOT EXISTS Country (
-    code            VARCHAR(3)  NOT NULL COMMENT 'Код страны',
-    name            VARCHAR(50) NOT NULL COMMENT 'Наименование страны',
+    code             VARCHAR(3)  NOT NULL COMMENT 'Код страны',
+    name             VARCHAR(50) NOT NULL COMMENT 'Наименование страны',
     PRIMARY KEY (code),
 );
 COMMENT ON TABLE Country IS 'Страна мира';
 CREATE INDEX IX_Country_Code ON Country(code);
 
-ALTER TABLE User ADD FOREIGN KEY (doc_code) REFERENCES Doc(code);
 ALTER TABLE User ADD FOREIGN KEY (citizenship_code) REFERENCES Country(code);
