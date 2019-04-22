6428cb7eb56d93441338f4e2ae60e0908a41a79a
Vasily Markov <vasily@markov.biz>
2019-04-22 19:02:49 +0300

    Добавлена возможность помещения JSON-объектов в обёртку

---
+++ src/main/java/com/bellintegrator/zirconium/controller/JSONResponseWrapper.java
@@ -0,0 +1,40 @@
+package com.bellintegrator.zirconium.controller;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+public class JSONResponseWrapper implements ResponseBodyAdvice {
+
+    @Override
+    public boolean supports(MethodParameter returnType, Class converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body,
+                                  MethodParameter returnType,
+                                  MediaType selectedContentType,
+                                  Class selectedConverterType,
+                                  ServerHttpRequest request,
+                                  ServerHttpResponse response) {
+
+        return new Wrapper<>(body);
+    }
+
+    private class Wrapper<T> {
+        private final T data;
+
+        public Wrapper(T o) {
+            this.data = o;
+        }
+
+        public T getData() {
+            return data;
+        }
+    }
+}
