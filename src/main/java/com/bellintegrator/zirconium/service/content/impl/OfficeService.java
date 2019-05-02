713dd8beedad13d6114de7855ef48492361d616c
Vasily Markov <vasily@markov.biz>
2019-05-02 21:29:23 +0300

    Удалены более неиспользуемые фрагменты кода

--- src/main/java/com/bellintegrator/zirconium/service/content/impl/OfficeService.java
+++ src/main/java/com/bellintegrator/zirconium/service/content/impl/OfficeService.java
@@ -7,37 +7,28 @@ import com.bellintegrator.zirconium.model.mapper.MapperFacade;
 import com.bellintegrator.zirconium.repository.OfficeRepository;
 import com.bellintegrator.zirconium.service.content.ContentService;
 import com.bellintegrator.zirconium.view.OfficeView;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
-
 
 /**
- * Сервис для работы с mock-объектами офисов
+ * Сервис для работы с объектами офисов
  */
 @Service("office")
 public class OfficeService implements ContentService<OfficeView> {
 
-    private final Map<Long, OfficeView> views = new HashMap<>();
-    private final AtomicLong counter = new AtomicLong();
-
-    private final ObjectMapper objectMapper;
     private final OfficeRepository officeRepository;
     private final MapperFacade mapperFacade;
 
     @Autowired
     public OfficeService(OfficeRepository officeRepository,
-                         MapperFacade mapperFacade,
-                         ObjectMapper mapper) {
+                         MapperFacade mapperFacade) {
 
         this.officeRepository = officeRepository;
         this.mapperFacade = mapperFacade;
-        this.objectMapper = mapper;
 
         mapperFacade.getMapperFactory()
                 .classMap(Office.class, OfficeView.class)
@@ -45,17 +36,6 @@ public class OfficeService implements ContentService<OfficeView> {
                 .fieldAToB("phone{phone}", "phone{}")
                 .byDefault()
                 .register();
-
-        OfficeView officeView = new OfficeView(
-                1L,
-                1L,
-                "Исследовательский центр",
-                "г. Москва, ул. Вербная, д. 5",
-                Arrays.asList("74957870544", "74957870545"),
-                true
-        );
-
-//        save(view);
     }
 
     /**
@@ -64,7 +44,6 @@ public class OfficeService implements ContentService<OfficeView> {
     @Override
     @Transactional
     public Collection<OfficeView> list(OfficeView officeView) {
-//        OfficeView officeView = deserialize(view);
         Office office = mapperFacade.map(officeView, Office.class);
 
         List<Office> offices = officeRepository.findAll(Example.of(office));
@@ -91,7 +70,6 @@ public class OfficeService implements ContentService<OfficeView> {
     @Override
     @Transactional
     public void update(OfficeView officeView) {
-//        OfficeView officeView = deserialize(view);
 
         long id = officeView.getId();
         Optional<Office> container = officeRepository.findById(id);
@@ -111,15 +89,10 @@ public class OfficeService implements ContentService<OfficeView> {
     @Override
     @Transactional
     public long save(OfficeView officeView) {
-//        OfficeView officeView = deserialize(view);
         Office office = mapperFacade.map(officeView, Office.class);
         Address address = new Address(officeView.getAddress());
         office.setAddress(address);
         officeRepository.save(office);
         return office.getId();
     }
-
-//    private OfficeView deserialize(Object json) {
-//        return objectMapper.convertValue(json, OfficeView.class);
-//    }
 }
