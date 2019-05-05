79882582ddb841339dbfdbfda84fbef7dc75578b
Vasily Markov <vasily@markov.biz>
2019-05-05 14:11:53 +0300

    Улучшен маппинг офисов.

--- src/main/java/com/bellintegrator/zirconium/service/content/impl/OfficeService.java
+++ src/main/java/com/bellintegrator/zirconium/service/content/impl/OfficeService.java
@@ -9,6 +9,9 @@ import com.bellintegrator.zirconium.repository.OfficeSpecification;
 import com.bellintegrator.zirconium.repository.PhoneRepository;
 import com.bellintegrator.zirconium.service.content.ContentService;
 import com.bellintegrator.zirconium.view.OfficeView;
+import ma.glasnost.orika.CustomConverter;
+import ma.glasnost.orika.MappingContext;
+import ma.glasnost.orika.metadata.Type;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +33,20 @@ public class OfficeService implements ContentService<OfficeView> {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    private class Converter extends CustomConverter<Set<String>, Set<Phone>> {
+        @Override
+        public Set<Phone> convert(Set<String> phones,
+                                  Type<? extends Set<Phone>> type,
+                                  MappingContext mappingContext) {
+
+            if (phones == null) {
+                return null;
+            }
+
+            return phones.stream().map(Phone::new).collect(Collectors.toSet());
+        }
+    }
+
     @Autowired
     public OfficeService(OfficeRepository officeRepository,
                          PhoneRepository phoneRepository,
@@ -39,9 +56,12 @@ public class OfficeService implements ContentService<OfficeView> {
         this.phoneRepository = phoneRepository;
         this.mapperFacade = mapperFacade;
 
+        mapperFacade.getMapperFactory().getConverterFactory().registerConverter("PhoneToPhone", new Converter());
+
         mapperFacade.getMapperFactory()
                 .classMap(Office.class, OfficeView.class)
                 .fieldAToB("phone{phone}", "phone{}")
+                .fieldMap("phone", "phone").converter("PhoneToPhone").bToA().add()
                 .byDefault()
                 .register();
     }
@@ -111,15 +131,6 @@ public class OfficeService implements ContentService<OfficeView> {
     @Transactional
     public long save(OfficeView officeView) {
         Office office = mapperFacade.map(officeView, Office.class);
-
-        Set<String> strPhones = officeView.getPhone();
-        if (strPhones != null) {
-            Set<Phone> phones = strPhones.stream()
-                                          .map(Phone::new)
-                                          .collect(Collectors.toSet());
-            office.setPhone(phones);
-        }
-
         officeRepository.save(office);
         return office.getId();
     }
