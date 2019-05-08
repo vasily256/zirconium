9b78347520c4cd962300c05ae784ddcb288fcdcb
Vasily Markov <vasily@markov.biz>
2019-05-08 21:35:48 +0300

    Добавлена обработка ошибок при валидации и прочих исключений.

--- src/main/java/com/bellintegrator/zirconium/controller/GlobalControllerExceptionHandler.java
+++ src/main/java/com/bellintegrator/zirconium/controller/GlobalControllerExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.bellintegrator.zirconium.controller;
 
 import com.bellintegrator.zirconium.exception.EntityNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 /**
@@ -12,13 +15,26 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  * соответствующих ответов сервера
  */
 @ControllerAdvice
-public class GlobalControllerExceptionHandler
-        extends ResponseEntityExceptionHandler {
+public class GlobalControllerExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+        ErrorResponseBody body = new ErrorResponseBody(ex.getMessage());
+        return new ResponseEntity<>(body, headers, status);
+    }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    protected ResponseEntity<?> handleException(RuntimeException ex) {
-
+    protected ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException ex) {
         ErrorResponseBody body = new ErrorResponseBody(ex.getMessage());
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    protected ResponseEntity<?> handleAllExceptions(Throwable ex) {
+        ErrorResponseBody body = new ErrorResponseBody(ex.getMessage());
+        return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
