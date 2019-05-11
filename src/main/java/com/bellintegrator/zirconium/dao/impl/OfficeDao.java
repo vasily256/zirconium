baa8afaef4c84540effdacd4d5443edc4616303d
Vasily Markov <vasily@markov.biz>
2019-05-11 11:10:31 +0300

    Изменения в OfficeDao

--- src/main/java/com/bellintegrator/zirconium/dao/impl/OfficeDao.java
+++ src/main/java/com/bellintegrator/zirconium/dao/impl/OfficeDao.java
@@ -2,13 +2,13 @@ package com.bellintegrator.zirconium.dao.impl;
 
 import com.bellintegrator.zirconium.dao.ContentDao;
 import com.bellintegrator.zirconium.dao.OfficeRepository;
-import com.bellintegrator.zirconium.dao.PhoneRepository;
 import com.bellintegrator.zirconium.exception.EntityNotFoundException;
 import com.bellintegrator.zirconium.model.Office;
 import com.bellintegrator.zirconium.model.Phone;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -16,13 +16,10 @@ import java.util.Set;
 @Repository
 public class OfficeDao implements ContentDao<Office> {
     private final OfficeRepository officeRepository;
-    private final PhoneRepository phoneRepository;
 
     @Autowired
-    public OfficeDao(OfficeRepository officeRepository,
-                     PhoneRepository phoneRepository) {
+    public OfficeDao(OfficeRepository officeRepository) {
         this.officeRepository = officeRepository;
-        this.phoneRepository = phoneRepository;
     }
 
     @Override
@@ -55,18 +52,22 @@ public class OfficeDao implements ContentDao<Office> {
             throw new EntityNotFoundException("can't update: office id " + id + " not found");
         }
 
-        Office officeDb = container.get();
+        Office currentOffice = container.get();
 
-        officeDb.setName(office.getName());
-        officeDb.setAddress(office.getAddress());
-        officeDb.setActive(office.isActive());
+        currentOffice.setName(office.getName());
+        currentOffice.setAddress(office.getAddress());
+        currentOffice.setActive(office.isActive());
 
-        Set<Phone> phones = office.getPhone();
-        if (phones != null) {
-            officeDb.getPhone().clear();
-            officeDb.setPhone(office.getPhone());
+        Set<Phone> newPhones = office.getPhone();
+        Set<Phone> currentPhones = new HashSet<>(currentOffice.getPhone());
+        if (newPhones != null) {
+            currentOffice.getPhone().clear();
+            currentOffice.getPhone().addAll(newPhones);
+            for (Phone phone : currentPhones) {
+                phone.getOffices().remove(currentOffice);
+            }
         }
 
-        officeRepository.save(officeDb);
+        officeRepository.save(currentOffice);
     }
 }
